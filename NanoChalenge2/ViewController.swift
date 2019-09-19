a9379113bfde2a852e15e0c35188d4b66d65f7c7
Nova Arisma <novaarisma95@gmail.com>
2019-09-19 13:17:40 +0700

    belajar CoreML

--- NanoChalenge2/ViewController.swift
+++ NanoChalenge2/ViewController.swift
@@ -7,14 +7,58 @@
 //
 
 import UIKit
+import AVKit
+import Vision
 
-class ViewController: UIViewController {
+class ViewController: UIViewController, AVCaptureVideoDataOutputSampleBufferDelegate{
 
     override func viewDidLoad() {
         super.viewDidLoad()
-        // Do any additional setup after loading the view.
+        
+        // here start up camera
+        let captureSession = AVCaptureSession()
+        captureSession.sessionPreset = .photo
+        
+        guard let captureDevice = AVCaptureDevice.default(for: .video) else { return }
+        guard let input = try? AVCaptureDeviceInput (device:captureDevice) else {return}
+        captureSession.addInput(input)
+        captureSession.startRunning()
+        
+        let previewLayer = AVCaptureVideoPreviewLayer(session : captureSession)
+        view.layer.addSublayer(previewLayer)
+        previewLayer.frame = view.frame
+        
+        let dataOutput = AVCaptureVideoDataOutput()
+        dataOutput.setSampleBufferDelegate( self, queue: DispatchQueue(label: "videoQueue"))
+        captureSession.addOutput(dataOutput)
+        
+        //let requiest = VNCoreMLRequest(model: <#T##VNCoreMLModel#>)
+        //VNImageRequestHandler(cgImage: <#T##CGImage#>, options: [:]).perform(<#T##requests: [VNRequest]##[VNRequest]#>)
+        
+    }
+    func captureOutput(_ output: AVCaptureOutput, didOutput sampleBuffer: CMSampleBuffer, from connection: AVCaptureConnection) {
+        //print("Camera was able to capture a frame:", Date())
+        
+        guard let pixxelBuffer: CVPixelBuffer = CMSampleBufferGetImageBuffer(sampleBuffer) else {return}
+        
+        guard let model = try? VNCoreMLModel(for: SqueezeNet().model) else {return}
+        let request = VNCoreMLRequest(model: model)
+        { (finishedReq, err) in
+            
+            //perhaps check the err
+            //print(finishedReq.results)
+            
+            guard let results = finishedReq.results as?
+                [VNClassificationObservation] else {return}
+            
+            guard let firstObservation = results.first else {return}
+            print(firstObservation.identifier, firstObservation.confidence)
+            
+        }
+        try? VNImageRequestHandler(cvPixelBuffer: pixxelBuffer, options: [:]).perform([request])
+    }
+    
     }
 
 
-}
 
